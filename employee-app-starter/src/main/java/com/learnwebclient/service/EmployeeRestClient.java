5ecd67ad346f6e5382e171d8e6891af87acb001f
Shi <stoneshishang@gmail.com>
2021-10-25 14:02:28 -0400

    add two http GET and its Junit tests

--- employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
+++ employee-app-starter/src/main/java/com/learnwebclient/service/EmployeeRestClient.java
@@ -1,4 +1,38 @@
 package com.learnwebclient.service;
 
+import com.learnwebclient.dto.Employee;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static com.learnwebclient.constants.EmployeeConstants.EMPLOYEE_BY_ID_V1;
+import static com.learnwebclient.constants.EmployeeConstants.GET_ALL_EMPLOYEES_V1;
+
 public class EmployeeRestClient {
+
+    private WebClient webClient;
+
+    public EmployeeRestClient(WebClient webClient){
+        this.webClient = webClient;
+    }
+
+//    http://localhost:8081/employeeservice/v1/allEmployees
+
+    public List<Employee> retrieveAllEmployees(){
+
+       return webClient.get().uri(GET_ALL_EMPLOYEES_V1)
+                .retrieve()
+                .bodyToFlux(Employee.class)
+                .collectList()
+                .block();
+    }
+
+    public Employee retrieveEmployeeById(int employeeId){
+
+       return webClient.get().uri(EMPLOYEE_BY_ID_V1, employeeId)
+                .retrieve()
+                .bodyToMono(Employee.class)
+                .block();
+    }
+
 }
--- employee-app-starter/src/test/java/com/learnwebclient/service/EmployeeRestClientTest.java
+++ employee-app-starter/src/test/java/com/learnwebclient/service/EmployeeRestClientTest.java
@@ -1,4 +1,33 @@
 package com.learnwebclient.service;
 
+import com.learnwebclient.dto.Employee;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class EmployeeRestClientTest {
+
+    private static final String baseUrl = "http://localhost:8081/employeeservice";
+    private WebClient webClient = WebClient.create(baseUrl) ;
+
+    EmployeeRestClient employeeRestClient = new EmployeeRestClient(webClient);
+
+    @Test
+    void retrieveAllEmployees(){
+        List<Employee> employeeList = employeeRestClient.retrieveAllEmployees();
+        System.out.println("empleeyList is: " + employeeList);
+        assertTrue(employeeList.size() > 0);
+    }
+
+    @Test
+    void retrieveEmployeeById(){
+
+        int employeeId = 1;
+        Employee employee = employeeRestClient.retrieveEmployeeById(employeeId);
+        assertEquals("Chris", employee.getFirstName());
+    }
 }
